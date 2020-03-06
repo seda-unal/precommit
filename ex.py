23b75a29cbb31fcf6f999186edaeb109bc514171
Seda Unal <sunal@metromile.com>
2020-03-05 21:26:43 -0800

    awesome code

--- ex.py
+++ ex.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
+
 ndf = pd.DataFrame()
-ndf['names'] = pd.Series(['Choco', 'Chica', 'Chico'])
+ndf["names"] = pd.Series(["Choco", "Chica", "Chico"])
 
 print(ndf)
-
-
-
-
+""" comments"""
